1c80bbb5a901e357cc91611ace48cabc09e408de
poojakakuru <poojareddykakuru@gmail.com>
2020-10-17 10:47:49 +0530

    "Welcome message is added"

---
+++ src/Program/EmployeeWage.java
@@ -0,0 +1,12 @@
+package Program;
+
+/**@author pooja kakuru
+ * Create Date   : 17th-october-2020
+ * Functionality : Calculating Employee Wage
+ */
+
+public class EmployeeWage {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee wage Computation program");
+    }
+}
