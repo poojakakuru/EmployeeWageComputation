7d834ef75266b7d600f8f7866fc78b0729e382af
poojakakuru <poojareddykakuru@gmail.com>
2020-10-17 11:54:09 +0530

    "Employeewage switch case is added"

--- src/Program/EmployeeWage.java
+++ src/Program/EmployeeWage.java
@@ -1,6 +1,8 @@
 package Program;
 import utility.Utility;
-/**@author Pooja Kakuru
+import java.util.Scanner;
+
+/**@author Pooja kakuru
  * Create Date   : 17th-october-2020
  * Functionality : Calculating Employee Wage
  */
@@ -9,25 +11,14 @@ public class EmployeeWage {
     public static void main(String[] args) {
         //welcome message
         System.out.println("Welcome to Employee Wage Computation Program");
+        //computing salary of employee using switch case
+        System.out.println(" To know the salary of employee , select any option below: ");
+        System.out.println("1. Full-time employee\n2. Part-time employee");
+        Scanner scanner=new Scanner(System.in);
+        int TypeofEmployee = scanner.nextInt();
         Utility util=new Utility();
-        //checking if employee is present or absent
-        double attendance = util.EmpAttendance();
-        if (attendance == 1)
-            System.out.println("Employee is Present");
-        else {
-            System.out.println("Employee is Absent");
-        }
-        //calculating daily wage for 8 hours full-time
-        int salary = util.DailyWage();
-        if (salary != 0)
-            System.out.println("salary for 8 hrs work daily is " + salary);
-        else
-            System.out.println("employee is absent ,hence salary is " +salary);
-        //calculating daily wage for 4 hours part-time
-        int salaryPartTime = util.DailyWagePartTime();
-        if (salaryPartTime != 0)
-            System.out.println("salary for 4 hrs part-time work is " + salaryPartTime);
-        else
-            System.out.println("employee is absent ,hence salary is " +salaryPartTime);
+        int salary=util.SwitchCaseComputation(TypeofEmployee);
+        System.out.println("Salary of the employee is " +salary);
+
     }
 }
--- src/utility/Utility.java
+++ src/utility/Utility.java
@@ -8,9 +8,11 @@ package utility;
  */
 public class Utility {
     int Is_present = 1;
+
     /**
-     *Functionality     : This function is used to generate
-     *                    random numbers
+     * Functionality     : This function is used to generate
+     *                     random numbers
+     *
      * @return empCheck - Returns generated random number
      */
 
@@ -18,10 +20,12 @@ public class Utility {
         double empCheck = Math.floor(Math.random() * 10) % 2;
         return empCheck;
     }
+
     /**
-     *Functionality : This function contains logic for checking
-     *                whether the employee is present or absent
-     * @return      - Return 1 if employee is present else returns 0
+     * Functionality : This function contains logic for checking
+     *                 whether the employee is present or absent
+     *
+     * @return       - Return 1 if employee is present else returns 0
      */
 
     public int EmpAttendance() {
@@ -31,41 +35,70 @@ public class Utility {
         else
             return 0;
     }
+
     /**
-     *Functionality   : This function is used to calculate
-     *                  salary for full time work of
-     *                  employee for 8 hours
-     * @return salary - Returns the value after computing the
-     *                  salary
+     * Functionality   : This function is used to calculate
+     *                   salary for full time work of
+     *                   employee for 8 hours
+     *
+     * @return salary  - Returns the value after computing the
+     *                   salary
      */
 
     public int DailyWage() {
-        int empRatePerHr=20;
-        int empHrs=8;
+        int empRatePerHr = 20;
+        int empHrs = 8;
         int salary;
         double attend = GenerateRandom();
         if (attend == Is_present)
-            return salary = empRatePerHr*empHrs;
+            return salary = empRatePerHr * empHrs;
+        else
+            return 0;
+    }
+
+    /**
+     * Functionality   : This function is used to calculate
+     *                   salary for part time work of
+     *                   employee for 4 hours
+     *
+     * @return salary  - Returns the value after computing the
+     *                   salary
+     */
+
+    public int DailyWagePartTime() {
+        int empRatePerHr = 20;
+        int empHrs = 4;
+        int salary;
+        double attend = GenerateRandom();
+        if (attend == Is_present)
+            return salary = empRatePerHr * empHrs;
         else
             return 0;
     }
     /**
-     *Functionality   : This function is used to calculate
-     *                  salary for part time work of
-     *                  employee for 4 hours
-     * @return salary - Returns the value after computing the
-     *                  salary
+     * Functionality   : This function is used to calculate
+     *                   salary of Full-time and Part-time
+     *                   work of employees using switch case
+     * @return salary  - Returns the value after computing the
+     *                   salary
      */
-
-    public int DailyWagePartTime() {
-        int empRatePerHr=20;
-        int empHrs=4;
+    public int SwitchCaseComputation(int TypeofEmployee) {
+        int empRateperHr = 20;
+        int empHrs;
         int salary;
-        double attend = GenerateRandom();
-        if (attend == Is_present)
-            return salary = empRatePerHr*empHrs;
-        else
-            return 0;
+        switch (TypeofEmployee) {
+            case 1:
+                empHrs = 8;
+                salary = empRateperHr * empHrs;
+                return salary;
+            case 2:
+                empHrs = 4;
+                salary = empRateperHr * empHrs;
+                return salary;
+            default:
+                return 0;
+
+        }
     }
 }
 
